50908ad990576cc4a71ac5a017401574ab0c0c6f
MichSeman <89609365+MichSeman@users.noreply.github.com>
2022-10-23 16:22:18 -0400

    html update
    removed last updated text - not needed

--- src/app.js
+++ src/app.js
@@ -4,15 +4,14 @@ function displayTemperature(response) {
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
 
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
-
     cityElement.innerHTML = response.data.name;
-
     descriptionElement.innerHTML = response.data.weather[0].description;
-
     humidityElement.innerHTML = response.data.main.humidity; 
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = "Friday 5:00";
 }
 
 let apiKey = "11e35b685db379570807b7fb5ef18ce6";
